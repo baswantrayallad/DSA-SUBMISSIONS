06b8707d25fb6a26784e5405a14e87f0a864a97c
Baswantray Allad <baswantray@gmail.com>
2026-04-14 23:21:55 +0530

    Add: maximum-points-you-can-obtain-from-cards - submission-0

---
+++ Data Structures & Algorithms/maximum-points-you-can-obtain-from-cards/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        if k == len(cardPoints):
+            return sum(cardPoints)
+
+        max_sum = 0
+        left_sum = 0
+        right_sum = 0
+        n = len(cardPoints)
+
+        for i in range(k):
+            left_sum += cardPoints[i]
+        max_sum = left_sum
+
+        right_index = n-1
+        for i in range(k-1,-1,-1):
+            left_sum -= cardPoints[i]
+            right_sum += cardPoints[right_index]
+            right_index -=1
+            max_sum = max(max_sum, left_sum + right_sum)
+        return max_sum
